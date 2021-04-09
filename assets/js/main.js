fa739d91afeca0728cfec2612589394b54238e94
Никита <59095759+nikita1007@users.noreply.github.com>
2021-04-09 21:13:26 +0500

    upload ( 9.04.21 )

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 function main() { 
   menuToggle()
   popup()
+  select()
 } main()
 
 // Menu Toggle
@@ -85,4 +86,34 @@ function popup() {
   popupCloseBtn.addEventListener('click', () => {
     popupWrap.style.cssText = `display: none;`
   })
+}
+
+// Select
+function select() {
+  const selectSingle = document.querySelector('.__select');
+  const selectSingle_title = selectSingle.querySelector('.__select__title');
+  const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+  // Toggle menu
+  selectSingle_title.addEventListener('click', () => {
+    if ('active' === selectSingle.getAttribute('data-state')) {
+      selectSingle.setAttribute('data-state', '');
+    } else {
+      selectSingle.setAttribute('data-state', 'active');
+    }
+  });
+
+  // Close when click to option
+  for (let i = 0; i < selectSingle_labels.length; i++) {
+    selectSingle_labels[i].addEventListener('click', (evt) => {
+      selectSingle_title.textContent = evt.target.textContent;
+      selectSingle.setAttribute('data-state', '');
+    });
+  }
+
+  // Reset title
+  const reset = document.querySelector('.reset');
+  reset.addEventListener('click', () => {
+    selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
+  });
 }
